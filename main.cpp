65da427bf053225efe3363bfe9c6aedda110554a
Florent Avellaneda <florent.avellaneda@gmail.com>
2025-05-07 19:21:08 -0400

    remove warning

--- main.cpp
+++ main.cpp
@@ -16,19 +16,6 @@ using namespace MaLib;
 
 Chrono TotalChrono("c Total time");
 
-std::string exec(std::string cmd) {
-    std::array<char, 128> buffer;
-    std::string result;
-    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd.c_str(), "r"), pclose);
-    if (!pipe) {
-        throw std::runtime_error("popen() failed!");
-    }
-    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
-        result += buffer.data();
-    }
-    return result;
-}
-
 std::string cur_file;
 std::unique_ptr<EvalMaxSAT<Solver_cadical>> monMaxSat;
 bool oldOutputFormat = false;
